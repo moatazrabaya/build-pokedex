9052ad4aecfd9810c52c6bdad563b96554d5867c
moatazrabaya <mutazrabaya203@gmail.com>
2026-02-12 23:32:22 +0200

    register help command

--- src/repl.ts
+++ src/repl.ts
@@ -2,7 +2,7 @@
 import readline from "readline";
 import type { CLICommand } from "./command.js";
 import { commandExit } from "./command_exit.js";
-
+import { commandHelp } from "./command_help.js";
 
 export function getCommands(): Record<string, CLICommand> {
   return {
