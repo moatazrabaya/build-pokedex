ec5e5024abb1e08e616e37e141e855dcc5cc0396
moatazrabaya <mutazrabaya203@gmail.com>
2026-02-14 19:11:26 +0200

    create State type

--- src/state.ts
+++ src/state.ts
@@ -1,4 +1,11 @@
 
+import { createInterface, type Interface } from "readline";
+
+export type State = {
+	rl: Interface;
+	registry: Record<string, CLICommand>;
+}
+
 export type CLICommand = {
   name: string;
   description: string;
