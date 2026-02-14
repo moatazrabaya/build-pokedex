53b2d81f31dde9d7550f23f1d5cc280d591aa7ce
moatazrabaya <mutazrabaya203@gmail.com>
2026-02-14 18:54:02 +0200

    update commandHelp function

--- src/command_help.ts
+++ src/command_help.ts
@@ -1,11 +1,12 @@
 import type { CLICommand } from "./command.js";
 
 export function commandHelp(commands: Record<string, CLICommand>) {
+  console.log();
   console.log("Welcome to the Pokedex!");
-  console.log("Usage:\n");
-
-  for (const key in commands) {
-    const cmd = commands[key];
+  console.log("Usage:");
+  console.log();
+  for (const cmd of Object.values(commands)) {
     console.log(`${cmd.name}: ${cmd.description}`);
   }
+  console.log();
 }
