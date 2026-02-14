8c00603e8660d8bb0f258eff8f19b2465e27be81
moatazrabaya <mutazrabaya203@gmail.com>
2026-02-14 18:52:46 +0200

    move the implenetation of getCommands function to commands.ts

--- src/repl.ts
+++ src/repl.ts
@@ -4,15 +4,6 @@ import type { CLICommand } from "./command.js";
 import { commandExit } from "./command_exit.js";
 import { commandHelp } from "./command_help.js";
 
-export function getCommands(): Record<string, CLICommand> {
-  return {
-    exit: {
-      name: "exit",
-      description: "Exits the pokedex",
-      callback: commandExit,
-    },
-  };
-}
 
 export function startREPL(){
 	const rl = readline.createInterface({
