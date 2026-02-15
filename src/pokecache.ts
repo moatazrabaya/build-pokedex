a830e294ad4a6ee55d9acc7058da06abb9fb685c
moatazrabaya <mutazrabaya203@gmail.com>
2026-02-15 03:21:43 +0200

    implement pokecache.ts structure

--- src/pokecache.ts
+++ src/pokecache.ts
@@ -1,1 +1,52 @@
 
+
+export type CacheEntry<T> = {
+	createdAt: number;
+	val: T;
+};
+
+export class Cache {
+	
+  #cache = new Map<string, CacheEntry<any>>();
+  #reapIntervalId: NodeJS.Timeout | undefined = undefined;
+  #interval: number;
+  
+  constructor(interval: number){
+	  this.#interval = interval;
+	  this.#startReapLoop();
+  }
+  
+  add<T> (key: string, val: T){
+	  const entry = {
+		  createdAt: Date.now(),
+		  val: val,
+	  }
+	  this.#cache.set(key, entry);
+  }
+  
+  get<T> (key: string){
+	  const entry = this.#cache.get(key);
+	  if(!entry)
+		  return undefined;
+	  return entry.val as T;
+  }
+  
+  #reap(){
+	  this.#cache.forEach((value, key) => {
+		  if(value.createdAt < Date.now() - this.#interval)
+			  this.#cache.delete(key);
+	  });
+  }
+  
+  #startReapLoop(){
+	  this.#reapIntervalId = setInterval(() => {
+		this.#reap();
+	  }, this.#interval);
+  }
+  
+  stopReapLoop(){
+	  clearInterval(this.#reapIntervalId);
+	  this.#reapIntervalId = undefined;
+  }
+}
+
