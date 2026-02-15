dd75a5a4ea10264a5236bfde9101800595e00c1b
moatazrabaya <mutazrabaya203@gmail.com>
2026-02-15 21:59:49 +0200

    update command_cached.ts

--- src/command_catch.ts
+++ src/command_catch.ts
@@ -13,20 +13,20 @@ export async function commandCatch(state: State, ...args: string[]): Promise<voi
 	  console.log(`Throwing a Pokeball at ${pokemonName}...`);
 
 	  try {
-		const pokemon = await state.api.fetchPokemon(pokemonName);
+			const pokemon = await state.api.fetchPokemon(pokemonName);
+			
+			// many base_experience values less than 600
+			const catchChance = 1 - pokemon.base_experience / 600;
+			const roll = Math.random();
 
-		// Catch formula
-		const catchChance = 1 - pokemon.base_experience / 600;
-		const roll = Math.random();
+			if (roll < catchChance) {
+			  console.log(`${pokemon.name} was caught!`);
 
-		if (roll < catchChance) {
-		  console.log(`${pokemon.name} was caught!`);
-
-		  // Add to pokedex
-		  state.catchedPokemons[pokemon.name] = pokemon;
-		} else {
-		  console.log(`${pokemon.name} escaped!`);
-		}
+			  state.catchedPokemons[pokemon.name] = pokemon;
+			} 
+			else {
+			  console.log(`${pokemon.name} escaped!`);
+			}
 	  } catch {
 		console.log("Pokemon not found.");
 	  }
